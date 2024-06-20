c9e468377c49846492fdfde682f437117beadc9b
Michael Ekstrand <md@ekstrandom.net>
2024-06-20 11:36:02 -0400

    import docs for version 0.12.1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.0',
+    VERSION: '0.12.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
-    BUILDER: 'readthedocs',
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
