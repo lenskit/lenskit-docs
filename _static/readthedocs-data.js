704b5d3122b9e39617406dc698d6f5a0337f5dcf
Michael Ekstrand <md@ekstrandom.net>
2024-06-20 11:35:55 -0400

    import docs for version 0.8.0

--- _static/readthedocs-data.js
+++ _static/readthedocs-data.js
@@ -1,6 +1,6 @@
 var READTHEDOCS_DATA = {
     project: "lkpy",
-    version: "0.7.0",
+    version: "0.8.0",
     language: "en",
     programming_language: "py",
     subprojects: {},
@@ -10,10 +10,11 @@ var READTHEDOCS_DATA = {
     docroot: "/doc/",
     source_suffix: ".rst",
     api_host: "https://readthedocs.org",
-    commit: "6514695d",
+    proxied_api_host: "https://readthedocs.org",
+    commit: "64c4e776",
     ad_free: false,
 
-    global_analytics_code: 'UA-17997319-1',
+    global_analytics_code: "UA-17997319-1",
     user_analytics_code: null
 };
 
