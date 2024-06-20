d6a171636e248ce5d2917fb3ccdbcb5279122324
Michael Ekstrand <md@ekstrandom.net>
2024-06-20 11:35:51 -0400

    import docs for version 0.2.0

---
+++ _static/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "lkpy",
+    version: "0.2.0",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "https://lkpy.readthedocs.io/en/stable/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/doc/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "58f19fe0",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: null
+};
+
