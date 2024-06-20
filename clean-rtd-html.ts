0e25fd3cc1d105962917d1f60185a5b9820b896b
Michael Ekstrand <md@ekstrandom.net>
2024-06-20 12:29:52 -0400

    walk and check

--- clean-rtd-html.ts
+++ clean-rtd-html.ts
@@ -1,6 +1,35 @@
-import { glob } from '@std/path'
-import { walk} from '@std/fs'
+import { assert } from "@std/assert";
+import { walk } from "@std/fs";
 
-import { DOMParser } from '@b-fuze/deno-dom';
+import { DOMParser } from "@b-fuze/deno-dom";
+import * as ai from "@hongminhee/aitertools";
+import docopt from "docopt";
 
-for await (let entry of walk('.'))
+let opts = docopt(`
+Clean RTD tags from documentation.
+
+Usage:
+    clean-rtd-html.ts [options] [DIR...]
+`);
+
+let dirs: string[];
+let diropt = opts["DIR"];
+assert(Array.isArray(diropt));
+if (diropt.length) {
+  dirs = diropt as string[];
+} else {
+  dirs = await ai.toArray(
+    ai.map(
+      (e: Deno.DirEntry) => `versions/${e.name}`,
+      ai.filter((e: Deno.DirEntry) => e.isDirectory, Deno.readDir("versions")),
+    ),
+  );
+}
+console.info("will work on %d version dirs: %o", dirs.length, dirs);
+
+for (let dir of dirs) {
+  console.info("cleaning %s", dir);
+  for await (let file of walk(dir, { exts: [".html"] })) {
+    console.debug("cleaning file %s", file.path);
+  }
+}
