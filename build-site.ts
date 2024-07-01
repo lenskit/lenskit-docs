1ec72840fea785c559a6920260c4171676e8b5f7
Michael Ekstrand <md@ekstrandom.net>
2024-07-01 14:22:59 -0400

    fix site build

--- build-site.ts
+++ build-site.ts
@@ -3,8 +3,6 @@ import { ensureDir } from "@std/fs";
 import * as fs from "node:fs";
 import * as git from "isomorphic-git";
 
-import * as ai from "@hongminhee/aitertools";
-
 let branches = await git.listBranches({ fs, dir: "." });
 branches = branches.filter((b) => b != "main");
 console.info("found %d versions: %o", branches.length, branches);
@@ -13,19 +11,19 @@ await ensureDir("site");
 
 for (let branch of branches) {
   console.info("extracting version %s", branch);
-  ensureDir(`site/${branch}`);
+  await ensureDir(`site/${branch}`);
   let archive = new Deno.Command("git", {
     args: ["archive", "--format=tar", branch],
     stdout: "piped",
   });
   let untar = new Deno.Command("tar", {
-    args: ["xvf", "-", "-C", `site/${branch}`],
+    args: ["xf", "-", "-C", `site/${branch}`],
     stdin: "piped",
   });
   let ar_proc = archive.spawn();
   let tar_proc = untar.spawn();
   await ar_proc.stdout.pipeTo(tar_proc.stdin);
-  let res = await ar_proc.output();
+  let res = await ar_proc.status;
   if (res.code != 0) {
     throw new Error(`git archive failed with code ${res.code}`);
   }
