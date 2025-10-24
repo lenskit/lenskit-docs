e604ed4eb2227b28eaf3e7b02881d11cac6f4400
Michael Ekstrand <mdekstrand@drexel.edu>
2025-10-24 12:27:13 -0400

    correcting branch

--- mise/tasks/ref-branches.sh
+++ mise/tasks/ref-branches.sh
@@ -5,7 +5,8 @@ set -e
 git fetch origin
 for ref in $(git branch -r --list 'origin/version/*'); do
     branch="${ref##origin/}"
-    if ! git show-ref $branch >/dev/null 2>&1; then
+    echo "checking branch $branch ($ref)"
+    if ! git show-ref --branches $branch >/dev/null 2>&1; then
         git branch -t $branch $ref
     fi
 done
